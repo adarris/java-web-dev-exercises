5dea2c9f29959eb39b429a9de7e85ca6ff2f4378
adrianadarris <adrianadarris@gmail.com>
2022-10-18 21:46:19 -0500

    finished studio

--- src/org/launchcode/java/studio/quizzes/Quiz.java
+++ src/org/launchcode/java/studio/quizzes/Quiz.java
@@ -1,4 +1,36 @@
 package org.launchcode.java.studio.quizzes;
 
+import java.util.ArrayList;
+
 public class Quiz {
+    private ArrayList<Question> questions;
+    private int score;
+    private int total;
+
+    public Quiz(ArrayList<Question> question){
+        this.questions = questions;
+        this.score = score;
+        this.total = total;
+        for (int i = 0; i < question.size(); i++){
+            this.total = this.total + questions.get(i).getPointValue();
+        }
+    }
+    public void addQuestion(Question newQuestion){
+        this.questions.add(newQuestion);
+        this.total = this.total + newQuestion.getPointValue();
+    }
+    public void runQuiz(){
+        for (Question question: this.questions){
+            question.displayQuestion();
+            question.displayAnswers();
+            int pts = question.getAnswers();
+            updateScore(pts);
+        }
+    }
+    public void updateScore(int x){
+        this.score = this.score + x;
+    }
+    public void gradeQuiz(){
+        System.out.println("You're done! Your score is " + score);
+    }
 }
--- src/org/launchcode/java/studio/quizzes/QuizRunner.java
+++ src/org/launchcode/java/studio/quizzes/QuizRunner.java
@@ -1,4 +1,5 @@
 package org.launchcode.java.studio.quizzes;
 
 public class QuizRunner {
+    // changes
 }
